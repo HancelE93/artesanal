3731998f36af1693536dd0f55175a2ffa24b5e79
HancelE1993 <hancelxyz@gmail.com>
2026-04-22 16:37:14 -0500

    4. Método agregarMaquina

--- src/com/krakedev/artesanal/NegocioMejorado.java
+++ src/com/krakedev/artesanal/NegocioMejorado.java
@@ -19,9 +19,28 @@ public class NegocioMejorado {
 	}
 
 	public String generarCodigo() {
-		int numero = (int) (Math.random() * 100) + 1;
-		String codigo = "M-" + numero;
-		return codigo;
-	}
 
+	    int numero = (int) (Math.random() * 100) + 1; 
+	    // Math.random() → da un decimal entre 0 y 1
+	    // *100 → lo pasa a 0–99
+	    // (int) → quita decimales (ej: 25.7 → 25)
+	    // +1 → ajusta el rango a 1–100
+
+	    String codigo = "M-" + numero; 
+	    // concatena texto + número → ej: "M-" + 25 = "M-25"
+
+	    return codigo; 
+	    // devuelve el resultado final
+	}
+	
+	public void agregarMaquina (String nombreCerveza,String descripcion,double precioPorMl) {
+		String codigo = generarCodigo(); // llamamos el metodo generar codigo y le guardamos en una variable
+		
+		Maquina maquina = new Maquina(nombreCerveza,descripcion,precioPorMl,codigo);
+		// se crea el objeto maquina y  con los valores que recibe
+		
+		maquinas.add(maquina); //agregamps el objeto
+		
+		
+	}
 }
